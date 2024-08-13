174a07e14316dedbe9c77cc866929d62a84cd3a6
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2024-08-13 13:02:01 +0100

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+﻿import React from 'react'
+import profilepic from "../assets/profilepic5.png"
+import {AiFillLinkedin, AiFillGithub} from "react-icons/ai";
+import { TypeAnimation } from "react-type-animation"
+
+const Hero = () => {
+    return (
+        <div>
+            <div className="bg-white max-w-[1200px] h-[80vh] mx-auto 
+            flex flex-col-reverse sm:flex-row
+            justify-center align-center">
+                
+                
+                <div className="text-8xl">
+                    Placeholder
+                </div>
+                
+                {/*cool picture*/}
+                <div className="my-auto">
+                    <img className="w-[300px] sm:w-[500px] mx-auto h-auto" src={profilepic} alt="profile pic" />
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Hero
